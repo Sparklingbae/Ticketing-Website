7a9420ddf4f784d555e73de8fd204d30055b46a1
Ridwan Akinola <akinolaridwanbnhusaain@gmail.com>
2024-05-30 23:06:55 +0100

    react file structure, css framework and client-side dependencies update

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { Navbar, Footer, EventList } from "./components"
+import React from 'react';
+
+const App = () => {
+
+  return (
+      <div className="App">
+        <Navbar />
+        <EventList />
+      </div>
+  )
+}
+
+export default App
---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar';
+export { default as EventList } from './EventList';
+export { default as Footer } from './Footer';
+export { default as React } from '@vitejs/plugin-react'
