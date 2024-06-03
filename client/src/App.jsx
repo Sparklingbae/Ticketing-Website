47aeacf0d253f616b587c54cb845118f3a9430ab
Ridwan Akinola <akinolaridwanbnhusaain@gmail.com>
2024-06-03 16:15:40 +0100

    Pages router: All pages has been separated and ready for development

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
-
-import { Navbar, EventList,Footer } from "./components"
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from "./components/Login"
 import SignUp from "./components/SignUp"
+import EventList from "./components/EventList"
+import Home from './components/Home'
 
 const App = () => {
 
   return (
       <div className="App">
-        <Navbar />
-        <EventList />
-        <Footer />
-        <Login />
-        <SignUp />
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} /> 
+            <Route path="/Login" element={<Login />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/create" element={<EventList />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/eventlist" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
       </div>
   
   )
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -23,6 +23,18 @@ const Login = () => {
           <body class="h-full">
           ```
         */}
+        <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/" className="text-white text-lg font-bold">EventPlug</a>
+        <div>
+          <a href="/" className="text-gray-300 hover:text-white mx-2">Home</a>
+          <a href="/eventlist" className="text-gray-300 hover:text-white mx-2">Event List</a>
+          <a href="/create" className="text-gray-300 hover:text-white mx-2">Create Event</a>
+          <a href="/login" className="text-gray-300 hover:text-white mx-2">Login</a>
+          <a href="/register" className="text-gray-300 hover:text-white mx-2">Sign Up</a>
+        </div>
+      </div>
+    </nav>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
