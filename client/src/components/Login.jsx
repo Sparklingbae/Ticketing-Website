ace0f050cf27597b45310b30025274e2797743d2
Ridwan Akinola <akinolaridwanbnhusaain@gmail.com>
2024-06-13 17:59:22 +0100

    login and signup api calls to backeend update

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,33 +1,41 @@
 /* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import HeroSection from "./HeroSection";
 
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 const Login = () => {
-    return (
-      <>
-        {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
-    <nav className="bg-gray-800 p-4">
-       <div className="container mx-auto flex justify-between items-center">
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:5000/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert('Login successful!');
+        // Save token, redirect the user, or perform other actions as needed
+      } else {
+        alert(`Login failed: ${data.msg}`);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again.');
+    }
+  };
+
+  return (
+    <>
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
           <a href="/" className="text-white text-lg font-bold">EventPlug</a>
           <div>
             <a href="/" className="text-gray-300 hover:text-white mx-2">Home</a>
@@ -37,80 +45,85 @@ const Login = () => {
             <a href="/login" className="text-gray-300 hover:text-white mx-2">Login</a>
             <a href="/register" className="text-gray-300 hover:text-white mx-2">Sign Up</a>
           </div>
-       </div>
-    </nav>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src=""
-              alt="EventPlug"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your EventPlug Account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-gray-800 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
-          </div>
         </div>
-      </>
-    )
-  }
+      </nav>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src=""
+            alt="EventPlug"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your EventPlug Account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleLogin}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="/register" className="font-semibold leading-6 text-gray-800 hover:text-indigo-500">
+              Sign up
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Login;
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,79 +1,184 @@
 /* eslint-disable no-unused-vars */
-import React from "react"
+import React, { useState } from 'react';
+
 const SignUp = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [gender, setGender] = useState('');
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:5000/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, username, email, phoneNumber, password, gender }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert('Registration successful!');
+        // You can redirect the user or clear the form here
+      } else {
+        alert(`Registration failed: ${data.msg}`);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again.');
+    }
+  };
+
   return (
-    <><nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-white text-lg font-bold">EventPlug</a>
-        <div>
-          <a href="/" className="text-gray-300 hover:text-white mx-2">Home</a>
-          <a href="/eventlist" className="text-gray-300 hover:text-white mx-2">Event List</a>
-          <a href="/create" className="text-gray-300 hover:text-white mx-2">Create Event</a>
-          <a href="/about" className="text-gray-300 hover:text-white mx-2">About</a>
-          <a href="/login" className="text-gray-300 hover:text-white mx-2">Login</a>
-          <a href="/register" className="text-gray-300 hover:text-white mx-2">Sign Up</a>
+    <>
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <a href="/" className="text-white text-lg font-bold">EventPlug</a>
+          <div>
+            <a href="/" className="text-gray-300 hover:text-white mx-2">Home</a>
+            <a href="/eventlist" className="text-gray-300 hover:text-white mx-2">Event List</a>
+            <a href="/create" className="text-gray-300 hover:text-white mx-2">Create Event</a>
+            <a href="/about" className="text-gray-300 hover:text-white mx-2">About</a>
+            <a href="/login" className="text-gray-300 hover:text-white mx-2">Login</a>
+            <a href="/register" className="text-gray-300 hover:text-white mx-2">Sign Up</a>
+          </div>
         </div>
-      </div>
-    </nav>
-    <div className=" mt-0.2 mb-0.2 container bg-white shadow-lg rounded-lg p-12">
-    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              SignUp to your EventPlug Account
-            </h2>
-        <form>
+      </nav>
+      <div className=" mt-0.2 mb-0.2 container bg-white shadow-lg rounded-lg p-12">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          SignUp to your EventPlug Account
+        </h2>
+        <form onSubmit={handleSignUp}>
           <div className="flex min-h-full flex-1 flex-col justify-center px-12 py-12 lg:px-12">
             <div className="input-box">
               <span className="details block font-semibold mb-2">Full Name</span>
-              <input type="text" placeholder="Enter your name" required className="w-full p-2 border border-gray-300 rounded" />
+              <input
+                type="text"
+                placeholder="Enter your name"
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="input-box">
               <span className="details block font-semibold mb-2">Username</span>
-              <input type="text" placeholder="Enter your username" required className="w-full p-2 border border-gray-300 rounded" />
+              <input
+                type="text"
+                placeholder="Enter your username"
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="input-box">
               <span className="details block font-semibold mb-2">Email</span>
-              <input type="email" placeholder="Enter your email" required className="w-full p-2 border border-gray-300 rounded" />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="input-box">
               <span className="details block font-semibold mb-2">Phone Number</span>
-              <input type="tel" placeholder="Enter your number" required className="w-full p-2 border border-gray-300 rounded" />
+              <input
+                type="tel"
+                placeholder="Enter your number"
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
             </div>
             <div className="input-box">
               <span className="details block font-semibold mb-2">Password</span>
-              <input type="password" placeholder="Enter your password" required className="w-full p-2 border border-gray-300 rounded" />
+              <input
+                type="password"
+                placeholder="Enter your password"
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className="input-box">
               <span className="details block font-semibold mb-2">Confirm Password</span>
-              <input type="password" placeholder="Confirm your password" required className="w-full p-2 border border-gray-300 rounded" />
+              <input
+                type="password"
+                placeholder="Confirm your password"
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
             </div>
           </div>
           <div className="gender-details my-0">
             <span className="gender-title block font-semibold mb-2">Gender</span>
             <div className="category flex justify-between items-center">
               <label className="flex items-center">
-                <input type="radio" name="gender" value="male" className="mr-2" />
+                <input
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  className="mr-2"
+                  checked={gender === 'male'}
+                  onChange={(e) => setGender(e.target.value)}
+                />
                 <span className="gender">Male</span>
               </label>
               <label className="flex items-center">
-                <input type="radio" name="gender" value="female" className="mr-2" />
+                <input
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  className="mr-2"
+                  checked={gender === 'female'}
+                  onChange={(e) => setGender(e.target.value)}
+                />
                 <span className="gender">Female</span>
               </label>
               <label className="flex items-center">
-                <input type="radio" name="gender" value="prefer-not-to-say" className="mr-2" />
+                <input
+                  type="radio"
+                  name="gender"
+                  value="prefer-not-to-say"
+                  className="mr-2"
+                  checked={gender === 'prefer-not-to-say'}
+                  onChange={(e) => setGender(e.target.value)}
+                />
                 <span className="gender">Prefer not to say</span>
               </label>
             </div>
           </div>
           <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Register
-                </button>
-              </div>
-            </form>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Register
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
-}
+};
+
 export default SignUp;
